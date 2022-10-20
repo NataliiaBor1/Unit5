3691876e4900554a294fe63975bc9d4e58f3d1b7
NataliiaBor1 <borisenko.nataliia1@gmail.com>
2022-10-19 18:34:33 -0700

    evening update

---
+++ src/ArrayStack.java
@@ -0,0 +1,59 @@
+import java.util.EmptyStackException;
+
+public class ArrayStack {
+    //Stack Implementation using Array (adapted from the implementation in the Udemy course):
+
+    //The runtime complexity of both functions is linear, as each it iterates through each character in the string.
+    // In each iteration, methods called like contains(), pop(), charAt(), and get() all take constant time.
+
+    private String[] stack;
+    private int top;
+
+    public ArrayStack(int capacity) {
+        stack = new String[capacity];
+    }
+
+    public void push(String s) {
+        if (top == stack.length) {
+            // need to resize the backing array
+            String[] newArray = new String[2 * stack.length];
+            System.arraycopy(stack, 0, newArray, 0, stack.length);
+            stack = newArray;
+        }
+
+        stack[top++] = s;
+    }
+
+    public String pop() {
+        if (isEmpty()) {
+            throw new EmptyStackException();
+        }
+
+        String s = stack[--top];
+        stack[top] = null;
+        return s;
+    }
+
+    public String peek() {
+        if (isEmpty()) {
+            throw new EmptyStackException();
+        }
+
+        return stack[top - 1];
+    }
+
+    public int size() {
+        return top;
+    }
+
+    public boolean isEmpty() {
+        return top == 0;
+    }
+
+    public void printStack() {
+        for (int i = top - 1; i >= 0; i--) {
+            System.out.println(stack[i]);
+        }
+    }
+
+}
--- src/BalancedParentheses.java
+++ src/BalancedParentheses.java
@@ -2,6 +2,7 @@ import java.util.HashMap;
 import java.util.Stack;
 
 public class BalancedParentheses {
+    //
 
     public static Boolean balancedParentheses(String s) {
         int countOpen = 0;
@@ -177,6 +178,56 @@ public class BalancedParentheses {
             return false;
         }
         return true;
+    }
+
+    public static boolean balancedParentheses4(String s) {   // from solution expanding
+        int numUnclosedOpenParens = 0;
+        for (int i = 0; i < s.length(); i += 1) {
+            if (s.charAt(i) == '(') {
+                numUnclosedOpenParens += 1;
+            }
+            else if (s.charAt(i) == ')') {
+                numUnclosedOpenParens -= 1;
+            }
+
+            if (numUnclosedOpenParens < 0) {
+                return false;
+            }
+        }
+
+        return numUnclosedOpenParens == 0;
+    }
+
+    public static boolean balancedBrackets(String s) {   // from solution expanding
+
+        Stack<Character> stack = new Stack<Character>();
+        HashMap<Character, Character> bracketPairs = new HashMap<Character, Character>();
+        bracketPairs.put(')', '(');
+        bracketPairs.put(']', '[');
+        bracketPairs.put('}', '{');
+        bracketPairs.put('>', '<');
+
+        for (int i = 0; i < s.length(); i += 1) {
+
+            // If character is opening bracket
+            if (bracketPairs.containsValue(s.charAt(i))) {
+                stack.push(s.charAt(i));
+            }
+            // Else, if character is closing bracket
+            else if (bracketPairs.containsKey(s.charAt(i))) {
+                // Closing bracket without matching opening bracket
+                if (stack.isEmpty()) {
+                    return false;
+                }
+                // Check that most recent bracket on stack matches
+                Character mostRecentBracket = stack.pop();
+                if (!mostRecentBracket.equals(bracketPairs.get(s.charAt(i)))) {
+                    return false;
+                }
+            }
+        }
+
+        return stack.isEmpty();
     }
 
     public static int postfix(String expression) {  // "2 3 1 * + 9 -"
